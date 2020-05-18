e5992218d52cd38ff0da0c14babbe7573f61433f
joyounghun <whdudgns2654@gmail.com>
2020-05-18 23:55:20 +0900

    스프링 활용 mvc 프로젝트 추가

---
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -0,0 +1,28 @@
+package me.whiteship.demowebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest
+class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void helloTest() throws Exception {
+        this.mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+
+}
