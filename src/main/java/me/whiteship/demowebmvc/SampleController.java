4b00327eba89dc20b67ddad6c830c063c9667c9b
joyounghun <whdudgns2654@gmail.com>
2020-05-19 18:27:02 +0900

    @Validated을 사용하여 유효성을 검증하는 그룹 사용
    스프링 버너 5부터는 bean validationd을 사용하려면 의존성을 추가해야한다.

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -3,6 +3,7 @@ package me.whiteship.demowebmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -18,7 +19,7 @@ public class SampleController {
 
     @PostMapping("/events")
     @ResponseBody
-    public Event getEvent(@ModelAttribute Event event, BindingResult bindingResult) {
+    public Event getEvent(@Validated(Event.ValidateLimit.class) @ModelAttribute Event event, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println("====================");
             bindingResult.getAllErrors().forEach(c -> {
