a10d4965cb2f42a6c7dcd6382fe42a90d79fd998
joyounghun <whdudgns2654@gmail.com>
2020-05-19 12:42:46 +0900

    http method에 따라 요청 매핑

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,14 +1,38 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class SampleController {
+
+    @GetMapping("/events")
+    @ResponseBody
+    public String events() {
+        return "events";
+    }
+
+    @GetMapping("/events/{id}")
+    @ResponseBody
+    public String getEvents(@PathVariable int id) {
+        return "events";
+    }
 
     @GetHelloMapping
     @ResponseBody
     public String hello() {
         return "hello";
     }
+
+
+
+    @DeleteMapping("/events/{id}")
+    @ResponseBody
+    public String removeEvents(@PathVariable int id) {
+        return "events";
+    }
+
+
 }
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -3,9 +3,10 @@ package me.whiteship.demowebmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,5 +24,58 @@ class SampleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
 
+    }
+
+    @Test
+    void getEvents() throws Exception {
+        this.mockMvc.perform(get("/events"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getEventsWithId() throws Exception {
+        this.mockMvc.perform(get("/events/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/events/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/events/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void createEvent() throws Exception {
+        this.mockMvc.perform(post("/events")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteEvent() throws Exception {
+        this.mockMvc.perform(delete("/events/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(delete("/events/2"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(delete("/events/3"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateEvent() throws Exception {
+        this.mockMvc.perform(put("/events")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
     }
 }
